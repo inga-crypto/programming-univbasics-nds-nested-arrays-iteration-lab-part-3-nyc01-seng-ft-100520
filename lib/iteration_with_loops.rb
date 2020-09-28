b50a7b4a54060e0f5414e78cc3fafecdaf80ac5a
Inga <ijaanimets@gmail.com>
2020-09-28 21:19:40 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -8,7 +8,7 @@ while row_index < src.count do
   element_index = 0
   while element_index < src[row_index].count do
     if src[row_index][element_index].class == String
-      src[row_index][element_index] = element
+      element = src[row_index][element_index]
    
   end
     
