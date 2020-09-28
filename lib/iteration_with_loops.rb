4b1fa652c5092fad5bae8d0e20ba181856c17409
Inga <ijaanimets@gmail.com>
2020-09-28 21:19:04 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -8,13 +8,13 @@ while row_index < src.count do
   element_index = 0
   while element_index < src[row_index].count do
     if src[row_index][element_index].class == String
-      
-   new_arr << element
+      src[row_index][element_index] = element
+   
   end
     
     element_index += 1
   end
   row_index += 1
 end
-new_arr
+new_arr << element
 end
